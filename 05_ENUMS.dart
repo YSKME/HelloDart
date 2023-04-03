cd77a2017a96756d0e32a9d3f5c66aa4e2e99605
YSKME YSKM <13930023364@163.com>
2023-04-03 03:34:13 +0000

    04/03/23

---
+++ 05_ENUMS.dart
@@ -0,0 +1,26 @@
+enum PlaneType{
+terrestrial,gas,ice
+}
+
+enumPlanet{
+  mercury(planetType:PlanetType.terrestrial,moons:0,hasRings:false),
+  venus(planetType:PlanetType.terrestrial,moons:0,hasRings:false),
+  //...
+  uranus(planetType:PlanetType.ice,moons:27,hasRings:false),
+  neptune(planetType:PlanetType.ice,moons:14,hasRIngs:true);
+  ///A constant generating constructor
+  const Planet(
+    {required this.planetType,required this.moons,required this.hasRings});
+    ///All instance variables are final
+    final PlanetType planetType;
+    final int moons;
+    final bool hasRings;
+    ///Enhanced enums support getters and other methods
+    bool get isGiant=>
+    planetType==PlanetType.gas||planetType ==PlanetType.ice;
+}
+
+final your Planet=Planet.earth;
+if(!yourPLanet.isGiant){
+  print('Your planet is not a "giant planet".');
+}
---
+++ 06_Inheritance.dart
@@ -0,0 +1,4 @@
+class Orbiter extends Spacecraft{
+  double altitude;
+  Orbiter(Super.name,DateTime super.launchDate,this.altitude);
+}
