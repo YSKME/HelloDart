929199f3ab98937eed7ad5789313c0a9a932e698
YSKME YSKM <13930023364@163.com>
2023-04-24 03:54:21 +0000

    4/24

--- tour_.dart
+++ tour_.dart
@@ -47,21 +47,36 @@ void main() {
   print(5);
   //从字符串中提取数据
   //Grab a substring.
- assert('Never odd or even'.substring(6, 9) == 'odd');
+  assert('Never odd or even'.substring(6, 9) == 'odd');
   //Split a string using a string pattern.
   var parts = 'progressive web apps'.split(' ');
   assert(parts.length == 3);
-  assert(parts[0]=='progressive');
+  assert(parts[0] == 'progressive');
   //Get a UTF-16 code ynit (as a string) by index.
-  assert('Never odd or even'[0]=='N');
-  //USe split() with an empty string parameter to get 
+  assert('Never odd or even'[0] == 'N');
+  //USe split() with an empty string parameter to get
   //a list of all characters (as Strings);good for
   //iterating.
-  for(final char in 'hello'.split('')){
+  for (final char in 'hello'.split('')) {
     print(char);
   }
 //Get all the UTF-16 code units in the string.
-var codeUnitList='Never odd or even'.codeUnits.toList();
-assert(codeUnitList[0]==78);
-
+  var codeUnitList = 'Never odd or even'.codeUnits.toList();
+  assert(codeUnitList[0] == 78);
+//Convert to uppercase.
+  assert('web apps'.toUpperCase() == 'WEB APPS');
+//Convert to lowercase.
+  assert('WEB APPS'.toLowerCase() == 'web apps');
+  //Trimming 和空字符串
+  //Trim a string.
+  assert('  hello  '.trim() == 'hello');
+  //Check whether a string is empty.
+  assert(''.isEmpty);
+  //Strings with only white space are not empty.
+  assert('   '.isNotEmpty);
+  //替换部分字符串
+  var greetingTemplate = 'Hello,NAME!';
+  var greeting = greetingTemplate.replaceAll(RegExp('NAME'), 'Bob');
+  //greetingTemplate didn't change.
+  assert(greeting != greetingTemplate);
 }
